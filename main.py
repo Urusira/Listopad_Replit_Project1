a9113edd5047bac4a3eb3e45f6df72d913dbb1bc
Urusira <102598467+Urusira@users.noreply.github.com>
2023-12-26 21:27:40 +0000

    Добавил тесты

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ def almost_palindrome(txt, n=0):
   for i in range(len(txt)):
       n += txt[i] != txt[::-1][i]
   return n == 2
-
-almost_palindrome("абвгвбж")
-# Если поменять «ж» на «а», получится палиндром «абвгвба».
