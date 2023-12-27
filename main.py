fa95e1b76f7e739a9425cf74aff744f6d3b9891a
Urusira <102598467+Urusira@users.noreply.github.com>
2023-12-27 20:21:16 +0000

    Поработал с Челом

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ def almost_palindrome(txt, n=0):
   for i in range(len(txt)):
       n += txt[i] != txt[::-1][i]
   return n == 2
+#мой комментарий оч важен!!!!
+#настолько важен, что прям очень важее
